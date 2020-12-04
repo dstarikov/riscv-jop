ebbc134c839e72865275a92d23feb915bfe4b42c
Keanu Vestil <keanu@cs.washington.edu>
2020-12-03 20:45:22 -0800

    Attempt to follow relative jumps

--- capstone-disas/rv_galileo.py
+++ capstone-disas/rv_galileo.py
@@ -9,31 +9,67 @@ import sys
 import re
 import math
 
-jmp_instr_pattern = re.compile("(c\.)?j(al)?(r)?")
+
+jmp_instr_pattern = re.compile(r"(c\.)?j(al)?(r)?")
+jmpi_instr_pattern = re.compile(r"(c\.)?j(al)?r")
+hex_pattern = re.compile(r"(?:-)?(?:0x)?[0-9a-f]+$")
 arch = CS_ARCH_RISCV
 mode = CS_MODE_RISCV64 | CS_MODE_RISCVC
 md = Cs(arch, mode)
 md.detail = True
+code = None
+
+
+def extract_hex(op_str):
+    h = hex_pattern.search(op_str)
+    if h is None:
+        print(op_str)
+        print(len(op_str))
+#    print(h.group())
+    return int(h.group(), 0)
+
+
+def is_indirect_jmp(instr_mnem):
+    return jmpi_instr_pattern.match(instr_mnem)
+
+
+def handle_indirect_jmp(addr):
+    for i in range(1, 5):
+        instr_seq = md.disasm(code[addr:], addr, i)
+        out = print_instr_seq(instr_seq, addr+(4*(i+1)))
+        if out is not None:
+            return out
 
 
 def print_instr_seq(instr_seq, end_addr):
     out = ""
-    last_mnem = None
+    last_instr = None
     for instr in instr_seq:
         out += "0x{:x}:\t".format(instr.address)
         out += "{}\t".format(bytes(instr.bytes).hex()) if instr.size == 4 else "{}    \t".format(bytes(instr.bytes).hex())
         out += "\t{}\t{}\t({} bytes)\n".format(instr.mnemonic, instr.op_str, instr.size)
-        last_mnem = instr.mnemonic
+        last_instr = instr
         # did we interpret past then actual jmp?
         if instr.address > end_addr:
             return
 
+    if last_instr is None:
+        return
+    
+    last_mnem = last_instr.mnemonic
     # do we still have a jmp at the end?
-    if jmp_instr_pattern.match(str(last_mnem)):
+    if last_mnem is not None and jmp_instr_pattern.match(last_mnem):
+        if not is_indirect_jmp(last_mnem):
+            imm = extract_hex(last_instr.op_str)
+            #print("relative jump!: {:x}".format(imm))
+            rel_jmp = handle_indirect_jmp(last_instr.address + imm)
+            if rel_jmp:
+                out += rel_jmp
+
         return out
 
 
-def telescope(code, idx, init_size, reg):
+def telescope(idx, init_size, reg):
     # riscv64gc allows for 16-bit and 32-bit instructions
     # this must be an even number
     max_size = 8
@@ -58,7 +94,7 @@ def main():
     if len(sys.argv) > 4:
         reg = sys.argv[4]
 
-    code = None
+    global code
     with open(file_name, mode="rb") as file:
         code = file.read()
 
@@ -70,7 +106,7 @@ def main():
         for instr in instr_seq:
             # are we looking at a jmp?
             if jmp_instr_pattern.match(instr.mnemonic):
-                telescope(code, idx, instr.size, reg)
+                telescope(idx, instr.size, reg)
         idx += 2
         instr_seq = md.disasm(code[idx:], idx, 1)
 
